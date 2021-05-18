85013e4d40751954a53b1c04a2d868b5ed98fd39
abhiramraghuraj <54733905+abhiramraghuraj@users.noreply.github.com>
2021-05-18 17:44:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var menu = document.querySelector('.icon').addEventListener('click',optionsfunc);
+var signin = document.querySelector('.member');
 function optionsfunc(){
     console.log("123");
     var vanish1 = document.querySelector('.showcase');
     var x = document.querySelector('.menu');
     //var show = document.querySelector('#list');
     if(x.className === 'menu'){
+        signin.style.display = 'none';
         var vanish1 = document.querySelector('.showcase');
         vanish1.style.display = 'none';
         x.className += ' active';
     }
     else{
+        signin.style.display = '';
         vanish1.style.display = '';
         x.className = 'menu';
     }
