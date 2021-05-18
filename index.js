ad43e39e288a21c7f6528ebcbd5548259b0e97dc
abhiramraghuraj <54733905+abhiramraghuraj@users.noreply.github.com>
2021-05-18 17:59:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var menu = document.querySelector('.icon').addEventListener('click',optionsfunc);
 var signin = document.querySelector('.member');
+var bars = document.querySelector('#bars');
 function optionsfunc(){
-    console.log("123");
     var vanish1 = document.querySelector('.showcase');
     var x = document.querySelector('.menu');
     //var show = document.querySelector('#list');
     if(x.className === 'menu'){
         signin.style.display = 'none';
+        bars.className = 'fa fa-close';
         var vanish1 = document.querySelector('.showcase');
         vanish1.style.display = 'none';
         x.className += ' active';
@@ -15,6 +16,7 @@ function optionsfunc(){
         signin.style.display = '';
         vanish1.style.display = '';
         x.className = 'menu';
+        bars.className = 'fa fa-bars';
     }
 
 }
